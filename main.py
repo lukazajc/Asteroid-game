f0417163539b16cda20bda2d29f5a59460ccc5eb
Luka <lukazajcster@gmail.com>
2025-01-01 09:35:11 +0100

    everything

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import pygame
 from constants import *
+from player import Player
+
+def main():
+        pygame.init()
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        dt = 0
+
+        while True:
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                return
+
+                player.update(dt)
+                screen.fill("black")
+                player.draw(screen)
+                pygame.display.flip()
+                clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()import pygame
+from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))	
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	dt = 0
-	
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-	
+
+		player.update(dt)	
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		clock.tick(60) / 1000
 
